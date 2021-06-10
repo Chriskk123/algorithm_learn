cb0332fa345a499ebd03f74e925bc9897b127e5e
Chris <1225437917@qq.com>
2021-06-10 08:20:20 +0800

    第九周作业

---
+++ week09/70爬楼梯.java
@@ -0,0 +1,44 @@
+package week09.leetcode.editor.cn;
+ 
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。 
+//
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？ 
+//
+// 注意：给定 n 是一个正整数。 
+//
+// 示例 1： 
+//
+// 输入： 2
+//输出： 2
+//解释： 有两种方法可以爬到楼顶。
+//1.  1 阶 + 1 阶
+//2.  2 阶 
+//
+// 示例 2： 
+//
+// 输入： 3
+//输出： 3
+//解释： 有三种方法可以爬到楼顶。
+//1.  1 阶 + 1 阶 + 1 阶
+//2.  1 阶 + 2 阶
+//3.  2 阶 + 1 阶
+// 
+// Related Topics 动态规划 
+// 👍 1678 👎 0
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2){
+            return n;
+        }
+        int a = 1,b = 2;
+        for(int i = 3;i<=n;i++){
+            int temp = b + a;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
